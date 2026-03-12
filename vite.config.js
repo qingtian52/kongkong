43b6dede8eac8dae986c37fd81c71822e21b9928
qingtian52 <942472269@qq.com>
2026-03-12 14:07:53 +0800

    fix: vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    port: 3000, // 自定义端口
-    open: true // 自动打开浏览器
-  },
-  assetsInclude: ['**/*.glb'] // 让 Vite 识别 GLB 模型文件
-});
+  // 关键：部署到 Vercel 需配置 base 为根路径
+  base: '/', 
+  // 如果是 GitHub Pages 才需要填仓库名，Vercel 直接用 '/' 即可
+  plugins: []
+})
